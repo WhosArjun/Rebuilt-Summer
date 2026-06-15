5cdc4a41de925676250a4a2b7eb15190740f7d8d
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-06-14 21:10:20 -0400

    vision more done

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -2,78 +2,59 @@ package frc.robot.subsystems.Vision;
 
 import org.littletonrobotics.junction.Logger;import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase{
     private final Camera shutter;
     private final Camera ardu;
-    private boolean visionEnabled = true;
+    private final SwerveDrivePoseEstimator poseEstimator; //Fuses odometry + vision
 
-    private final Consumer<VisionReadings> visionConsumer;
-    public Vision(Consumer<VisionReadings> visionConsumer){
-        this.visionConsumer = visionConsumer;
-        shutter = new Camera("Shutter623", new Transform3d(0.67,0.67,0.67, new Rotation3d(0, Math.toRadians(0.67), Math.toRadians(0.67))));
-        ardu = new Camera("Ardu623", new Transform3d(0.67,0.67,0.67, new Rotation3d(0, Math.toRadians(0.67), Math.toRadians(0.67))));
+    public Vision(SwerveDrivePoseEstimator poseEstimator){
+        this.poseEstimator = poseEstimator;
+        shutter = new Camera("Shutter623", new Transform3d(0.67,0.67,0.67, new Rotation3d(0,Math.toRadians(67), Math.toRadians(67))));
+        ardu = new Camera("Ardu623", new Transform3d(0.67,0.67,0.67, new Rotation3d(0,Math.toRadians(67), Math.toRadians(67))));
+
     }
 
     @Override
     public void periodic(){
-        SmartDashboard.putBoolean("Vision Enabled", visionEnabled);
-        if(!visionEnabled){
-            Logger.recordOutput("Shutter connected", shutter.isCameraConnected());
-            Logger.recordOutput("Ardu Connected", ardu.isCameraConnected());
+        List<VisionMeasurement> all = new ArrayList<>(); //Collects all vision measurements from camera
+        all.addAll(shutter.estimatePose());
+        all.addAll(ardu.estimatePose());
 
-            List<VisionReadings> allReadings = collectAllReadings();
-            Logger.recordOutput("Vision Reading Count", allReadings.size());
 
-            VisionReadings best = selectBestReading(allReadings);
+        //How many vision measurements we got this cycle
+        Logger.recordOutput("Vision Measurement Count", all.size());
 
-            if(best!=null){
-                Logger.recordOutput("Vision Best Position", best.getPose2d());
-                Logger.recordOutput("Vision best timeStamp", best.getTimestampSeconds());
-                Logger.recordOutput("Vision best camera", best.getName());
-                Logger.recordOutput("Vision best tagCount", best.getTagCount());
-                Logger.recordOutput("Vision Best ambiguity", best.getAmbiguity());
-
-                visionConsumer.accept(best); 
+        for(VisionMeasurement m : all){ //Proccesses each vision measurement individually 
+            if(m.confidence<0.5){
+                Logger.recordOutput("Vision/Rejected" + m.cameraName, true);
             }
+
+            Logger.recordOutput("Vision Accepted" + m.cameraName, true);
+
+            double stdDev = MathUtil.clamp(1.0/m.confidence, 0.05, 2.0); //Converts confidence into uncertainty, higher confidence -> lower noise
+
+            poseEstimator.addVisionMeasurement(m.pose.toPose2d(), m.timeStamp, VecBuilder.fill(stdDev, stdDev, stdDev)); //Feed vision measurement into pose estimator
+
         }
 
+        Pose2d pose = poseEstimator.getEstimatedPosition(); //Logs final fused robot pose (odometry + vision)
+        Logger.recordOutput("Vision Pose", pose);
     }
 
-    private List<VisionReadings> collectAllReadings(){
-            List<VisionReadings> readings = new ArrayList<>();
-            readings.addAll(shutter.estimatePose());
-            readings.addAll(ardu.estimatePose());
-            return readings;
-    }
 
-    private VisionReadings selectBestReading(List<VisionReadings> readings){
-        VisionReadings best = null;
-        double bestScore = Double.NEGATIVE_INFINITY;
-        for(VisionReadings r : readings){
-            double score = r.getTagCount()/(1+r.getAmbiguity());
-            if(score>bestScore){
-                bestScore = score;
-                best = r;
-            }
-        }
-        return best;
-    }
-
-    public void setVisionEnabled(boolean enabled){
-        this.visionEnabled = enabled;
-    }
-
-    public boolean isVisionEnabled(){
-        return visionEnabled;
+    //robot pose
+    public Pose2d getPose(){
+        return poseEstimator.getEstimatedPosition();
     }
     
 }
