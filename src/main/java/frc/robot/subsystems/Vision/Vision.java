17a721ff013da27cd1c260c37f7a927428c8b821
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-06-15 20:32:10 -0400

    vision v2

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -5,56 +5,49 @@ import java.util.List;
 
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Drivetrain;
 
 public class Vision extends SubsystemBase{
+    private final Drivetrain drivetrain;
     private final Camera shutter;
     private final Camera ardu;
-    private final SwerveDrivePoseEstimator poseEstimator; //Fuses odometry + vision
-
-    public Vision(SwerveDrivePoseEstimator poseEstimator){
-        this.poseEstimator = poseEstimator;
-        shutter = new Camera("Shutter623", new Transform3d(0.67,0.67,0.67, new Rotation3d(0,Math.toRadians(67), Math.toRadians(67))));
-        ardu = new Camera("Ardu623", new Transform3d(0.67,0.67,0.67, new Rotation3d(0,Math.toRadians(67), Math.toRadians(67))));
-
+    
+    public Vision(Drivetrain drivetrain){
+        this.drivetrain = drivetrain;
+        Transform3d camPose = new Transform3d(0.2,0.2,0.3, new Rotation3d(0,Math.toRadians(25),0));
+        shutter = new Camera("Shutter623", camPose);
+        ardu = new Camera("Ardu623", camPose);
     }
 
     @Override
     public void periodic(){
-        List<VisionMeasurement> all = new ArrayList<>(); //Collects all vision measurements from camera
-        all.addAll(shutter.estimatePose());
-        all.addAll(ardu.estimatePose());
+        List<VisionMeasurement> all = new ArrayList<>();
+        all.addAll(shutter.estimatPose());
+        all.addAll(ardu.estimatPose());
 
+        Logger.recordOutput("Vision/Measurement Count", all.size());
 
-        //How many vision measurements we got this cycle
-        Logger.recordOutput("Vision Measurement Count", all.size());
-
-        for(VisionMeasurement m : all){ //Proccesses each vision measurement individually 
+        for(VisionMeasurement m : all){
             if(m.confidence<0.5){
-                Logger.recordOutput("Vision/Rejected" + m.cameraName, true);
+                Logger.recordOutput("Vision" + m.cameraName + "Rejected", true);
+                continue;
             }
 
-            Logger.recordOutput("Vision Accepted" + m.cameraName, true);
+            Logger.recordOutput("Vision" + m.cameraName + "Accepted", true);
 
-            double stdDev = MathUtil.clamp(1.0/m.confidence, 0.05, 2.0); //Converts confidence into uncertainty, higher confidence -> lower noise
+            double stdDev = MathUtil.clamp(1.0/m.confidence, 0.05, 2.0);
 
-            poseEstimator.addVisionMeasurement(m.pose.toPose2d(), m.timeStamp, VecBuilder.fill(stdDev, stdDev, stdDev)); //Feed vision measurement into pose estimator
+            drivetrain.addVisionMeasurement(m.pose.toPose2d(), m.timeStamp, stdDev);
 
         }
-
-        Pose2d pose = poseEstimator.getEstimatedPosition(); //Logs final fused robot pose (odometry + vision)
-        Logger.recordOutput("Vision Pose", pose);
-    }
-
-
-    //robot pose
-    public Pose2d getPose(){
-        return poseEstimator.getEstimatedPosition();
+        Logger.recordOutput("Vision Position", drivetrain.getPose());
     }
     
+    public Pose2d getPose(){
+        return drivetrain.getPose();
+    }
 }
