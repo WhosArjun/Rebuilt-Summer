9e2dd210938d120e0e7c963377bd18d91e834350
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-06-15 20:31:51 -0400

    vision v2

---
+++ src/main/java/frc/robot/subsystems/Vision/VisionMeasurement.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.Vision;
+
+import edu.wpi.first.math.geometry.Pose3d;
+
+public class VisionMeasurement {
+    public final Pose3d pose; //Robot pose estimated from camera (3d space)
+    public final double timeStamp; //When this measurement was taken (latency correction)
+    public final double confidence; //How much we trust this measurement 
+    public final String cameraName; //Which camera produced the measurement
+
+    public VisionMeasurement(Pose3d pose, double timestamp, double confidence, String name){
+        this.pose = pose;
+        this.timeStamp = timestamp;
+        this.confidence = confidence;
+        this.cameraName = name;
+    }
+}
