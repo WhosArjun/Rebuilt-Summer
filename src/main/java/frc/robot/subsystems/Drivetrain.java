ef44f15057e8ec42d0c8aab1dbbb422470348848
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-06-15 20:31:27 -0400

    vision v2

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,8 +9,10 @@ import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -25,6 +27,7 @@ import swervelib.parser.SwerveParser;
 
 public class Drivetrain extends SubsystemBase{
     public SwerveDrive swerveDrive;
+    public final SwerveDrivePoseEstimator poseEstimator;
     
     public Drivetrain() {
         try{swerveDrive = new SwerveParser(new File(Filesystem.getDeployDirectory(),"swerve")).createSwerveDrive(Constants.MAX_SPEED,new Pose2d());
@@ -32,6 +35,7 @@ public class Drivetrain extends SubsystemBase{
         e.printStackTrace();
         throw new RuntimeException("67 yanis");
     }
+    poseEstimator = new SwerveDrivePoseEstimator(swerveDrive.kinematics, getGyroRotation(), swerveDrive.getModulePositions(), new Pose2d());
     configureAuto();
         
     }
@@ -44,6 +48,7 @@ public class Drivetrain extends SubsystemBase{
         catch(Exception e){
             throw new RuntimeException("Failed to grab Pathplanner");
         }
+
 
         AutoBuilder.configure(
             this::getPose,
@@ -61,12 +66,22 @@ public class Drivetrain extends SubsystemBase{
         );
     }
 
+    public Rotation2d getGyroRotation(){
+        return swerveDrive.getYaw();
+    }
+
+    public void addVisionMeasurement(Pose2d pose, double timestamp, double stdDev){
+        swerveDrive.addVisionMeasurement(pose, timestamp, VecBuilder.fill(stdDev,stdDev,stdDev));
+    }
+
     public Pose2d getPose(){
-        return swerveDrive.getPose();
+        return poseEstimator.getEstimatedPosition();
     }
 
     public void resetPose(Pose2d pose){
         swerveDrive.resetOdometry(pose);
+
+        poseEstimator.resetPosition(getGyroRotation(), swerveDrive.getModulePositions(), pose);
     }
 
     public ChassisSpeeds getRobotRelativeSpeeds(){
@@ -79,5 +94,9 @@ public class Drivetrain extends SubsystemBase{
 
     public String toString(){
         return "Drivetrain";
+    }
+
+    public void updateOdom(){
+        poseEstimator.update(getGyroRotation(), swerveDrive.getModulePositions());
     }
 }
