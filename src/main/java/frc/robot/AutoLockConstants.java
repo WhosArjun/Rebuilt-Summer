6ddd2993ed26de19009f86e64f563aa16f53b353
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-08-09 13:42:20 -0400

    working on autolock

---
+++ src/main/java/frc/robot/AutoLockConstants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class AutoLockConstants {
+    public static final double kP = 4.0;
+    public static final double kI = 0.0;
+    public static final double kD = 0.1;
+
+    public static final double ANGLE_TOLERANCE_DEGREES = 2.0;
+}
--- src/main/java/frc/robot/commands/AutoLock.java
+++ src/main/java/frc/robot/commands/AutoLock.java
@@ -1,7 +1,62 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.AutoLockConstants;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
+import swervelib.simulation.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
 
 public class AutoLock extends Command{
-    
+    private final Drivetrain drivetrain;
+    private final DoubleSupplier xTranslationSupplier;
+    private final DoubleSupplier yTranslationSupplier;
+
+    private final PIDController thetaController;
+
+    public AutoLock(Drivetrain drivetrain, DoubleSupplier xTranslationSupplier, DoubleSupplier yTranslationSupplier){
+        this.drivetrain = drivetrain;
+        this.xTranslationSupplier = xTranslationSupplier;
+        this.yTranslationSupplier = yTranslationSupplier;
+
+        thetaController = new PIDController(AutoLockConstants.kP,
+                                            AutoLockConstants.kI,
+                                            AutoLockConstants.kD);
+
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        thetaController.setTolerance(Math.toRadians(AutoLockConstants.ANGLE_TOLERANCE_DEGREES));
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize(){
+        thetaController.reset();
+    }
+
+
+    @Override
+    public void execute(){
+        Pose2d robotPose = drivetrain.getPose();
+        Translation2d target;
+
+        if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Red){
+            target = Constants.redHub; 
+        } else {
+            target = Constants.blueHub; 
+        }
+
+        Translation2d robotPosition = robotPose.getTranslation();
+        Translation2d difference = target.minus(robotPosition);
+
+        double currentAngle = robotPose.getRotation().getRadians();
+    }
+
 }
