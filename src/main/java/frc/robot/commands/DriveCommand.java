2e028742c06aae305655297ad27b5ea0b24763a9
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-09-24 01:49:19 -0400

    changed pid controller values

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -16,8 +16,8 @@ import frc.robot.subsystems.Drivetrain;
 import swervelib.math.SwerveMath;
 
 public class DriveCommand extends Command{
-    private Drivetrain drivetrain;
     private Supplier<Boolean> autoShooter;
+    private Drivetrain drivetrain;
     private DoubleSupplier xTranslationSupplier;
     private DoubleSupplier yTranslationSupplier;
     private DoubleSupplier thetaTranslationSupplier;
@@ -33,7 +33,8 @@ public class DriveCommand extends Command{
                             this.yTranslationSupplier = yTranslationSupplier;
                             this.thetaTranslationSupplier = thetaTranslationSupplier;
                             this.autoShooter = autoShooter;
-                            pidController = new PIDController(0.072,0,0);
+                            //pidController = new PIDController(5, 0, 0);
+                            pidController = new PIDController(0.072, 0, 0);
 
     }
 
@@ -64,17 +65,15 @@ public class DriveCommand extends Command{
 
     //If the joystick button is on you run the autolock if statement
     public double getThetaVelocity(){
-        boolean autoLockOn = autoShooter.get();
-        SmartDashboard.putBoolean("Auto Lock On", autoLockOn);
-        if (autoLockOn){
+        SmartDashboard.putBoolean("buttonOn", autoShooter.get());
+        if(autoShooter.get() == true){
             double thetaError = drivetrain.getHeadingError();
-            SmartDashboard.putNumber("Theta Error", thetaError);
-            double thetaVelocity = pidController.calculate(thetaError, 0);
-            SmartDashboard.putNumber("Theta Velocity", thetaVelocity);
-            return -thetaVelocity;
+            SmartDashboard.putNumber("theta error", thetaError);
+            double thetaOutput = pidController.calculate(thetaError, 0);
+            return -thetaOutput; //test this
         }
         else{
-            return deadzone(thetaTranslationSupplier.getAsDouble(),0.05)*Math.abs(drivetrain.swerveDrive.getMaximumChassisAngularVelocity());
+            return deadzone(thetaTranslationSupplier.getAsDouble(),0.05) * Math.abs(drivetrain.swerveDrive.getMaximumChassisAngularVelocity());
         }
         
     }
